0c23a18d14620b60368b7194518d7342d9b24b6c
heiwa4126 <heiwa4126@gmail.com>
2025-11-17 17:23:03 +0900

    fix version code

--- src/bumpuv/__init__.py
+++ src/bumpuv/__init__.py
@@ -3,6 +3,4 @@ from importlib.metadata import version
 from ._core import VersionInfo, bumpuvError, update_version
 
 __all__ = ["update_version", "VersionInfo", "bumpuvError"]
-
-
-__version__ = version("bumpuv")  # Python 3.9+ only
+__version__ = version(__package__ or __name__)  # Python 3.9+ only
